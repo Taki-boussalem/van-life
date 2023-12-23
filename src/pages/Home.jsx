bffaa3b26d560192f08561916bee7db126f226f0
Taki-boussalem <132174677+Taki-boussalem@users.noreply.github.com>
2023-12-23 21:42:33 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ export function Home() {
     <div className="home">
       <h1>You got the travel plans, we got the travel vans.</h1>
       <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+      <p>this is something I really want to add to test the git prop</p>
       <button className="find-your-van-btn">Find your van</button>
     </div>
   )
